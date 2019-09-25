68252b2513d270e3c8db5320ef954a2670f27518
Julien Sarazin <julien.sarazin@me.com>
2019-09-25 22:34:45 +0200

    added missing exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './interfaces';
 export * from './nest-http.module';
 export * from './decorators/HTTPClient.decorator';
 export * from './decorators/HTTPClientBuilder.decorator';
+export * from './classes/NHCInstance';
